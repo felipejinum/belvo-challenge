709314b07e4b37c9aa49843739f8a3695e42bcc7
felipe.um <felipe.um@piposaude.com.br>
2022-11-27 16:56:19 -0300

    Update per-capta-covid19-total-growth-by-country-per-week.sql

--- part-2/per-capta-covid19-total-growth-by-country-per-week.sql
+++ part-2/per-capta-covid19-total-growth-by-country-per-week.sql
@@ -12,7 +12,7 @@ select date_trunc(date,week) as week
 , round(safe_divide(max(confirmed),max(midyear_population)),6) as confirmed_cases_per_capta
 from covid19_country_summary
 left join `bigquery-public-data.census_bureau_international.midyear_population`
-on country_region = country_name
+on (country_region = country_name OR country_region = country_code)
 and extract(YEAR from date_trunc(date,week)) = year
 group by 1,2
 order by 1,2
